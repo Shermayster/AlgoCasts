1acfbdab9654377c03b2f3852ec05da53cb0b5e7
pavelshermayster <sherm84@gmail.com>
2018-07-28 09:16:23 +0300

    int reversal solution

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,26 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  const result =
+    n.toString()
+    .split('')
+    .reverse()
+    .join('')
+  return parseInt(result) * Math.sign(n);
+}
+console.log(Number('-5'))
+
+console.log(reverseInt(0) === 0);
+console.log(reverseInt(5) === 5);
+console.log(reverseInt(15) === 51);
+console.log(reverseInt(90) === 9);
+console.log(reverseInt(2359) === 9532);
+
+
+console.log(reverseInt(-5) === -5);
+console.log(reverseInt(-15) === -51);
+console.log(reverseInt(-90) === -9);
+console.log(reverseInt(-2359) === -9532);
 
 module.exports = reverseInt;
